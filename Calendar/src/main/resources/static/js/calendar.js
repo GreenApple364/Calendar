f180fe523d572c058fee984996e7808e7d9b0bf0
ryohei <ryohei@192.168.11.2>
2017-06-06 07:05:04 +0900

    画面ボタン配置とフォームの配置を調整

--- Calendar/src/main/resources/static/js/calendar.js
+++ Calendar/src/main/resources/static/js/calendar.js
@@ -1,5 +1,9 @@
-$(function(){
-	$('#plusNewEventForm').click(function(){
-		$('.newEventZone').append("<p>a</p>")
-	});
+$(function() {
+	$('#plusNewEventForm').click(
+			function() {
+				$('.newEventZone').find('tbody').append(
+						"<tr><td><input type='date' name='date'></td>"
+								+ "<td><input type='text' name='event'></td>"
+								+ "</tr>")
+			});
 });
