bb035909eb682e3f62d00743b591ff489253e9b9
9700170 <9700170@J9700170-2.wb.local>
2017-06-14 12:53:19 +0900

    予定追加のフォームを増やすボタンの配置をわかりやすい場所に移動/フォーム右の×をクリックするとその行のフォームが削除される（予定）

--- Calendar/src/main/resources/static/js/calendar.js
+++ Calendar/src/main/resources/static/js/calendar.js
@@ -3,22 +3,24 @@ $(function() {
 	$('#plusNewEventForm').click(
 			function() {
 				$('.newEventZone').find('tbody').append(
-						"<tr>" +
-							"<td><input type='date' name='newEvents[" + counter + "].eventDate'></td>" +
-							"<td><input type='text' name='newEvents[" + counter + "].event'></td>" + 
-						"</tr>"
-				);
+						"<tr>" + "<td><input type='date' name='newEvents["
+								+ counter + "].eventDate'></td>"
+								+ "<td><input type='text' name='newEvents["
+								+ counter + "].event'></td>" + "<td>×</td>"
+								+ "</tr>");
 				counter += 1
 			});
-	
-	$('#calendarZone td').hover(
-			function(){
-				$(this).find("form").append("<button>削除</button>");
-			},
-			function(){
-				$(this).find("button:last").remove();
-			}
-	);
+
+	$('#calendarZone td').hover(function() {
+		$(this).find("form").append("<button>削除</button>");
+	}, function() {
+		$(this).find("button:last").remove();
+	});
+
+	// TODO:作成されたフォームごとにイベントリスナを付与して削除ボタンを押下した際に任意の行を削除できるようにする.
+	$('.newEventZone tr>td:last').click(function() {
+		alert('a')
+	});
+
 });
 
-//TODO:作成されたフォームごとにイベントリスナを付与して削除ボタンを押下した際に任意の行を削除できるようにする.
